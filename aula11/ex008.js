5860cd78908d912141540677b5f92982c5c3a1f4
vismartins <131977004+vismartins@users.noreply.github.com>
2023-06-06 15:54:11 -0300

    ex010

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel=50
+console.log(`A velocidade do carro é ${vel}Km/hr.`)
+if (vel > 60){
+    console.log(`Você ultrapassou o limite de velocidade.MULTADO!`)
+} else {
+console.log(`Você esta dirigindo dentro do limite de velocidade. Boa viagem!`)
+}
