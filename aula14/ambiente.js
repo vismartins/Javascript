2de07977ffd22445883cae52619e653b88a425d1
vismartins <131977004+vismartins@users.noreply.github.com>
2023-06-29 13:39:14 -0300

    att

--- aula14/ambiente.js
+++ aula14/ambiente.js
@@ -2,8 +2,18 @@
 while(c <= 5){
     console.log(c)
     c++
-}*/
+}
 
 for(var c=1;c<=10;c++){
     console.log(c)
+}*/
+
+let valores = [2, 6, 9, 7, 1, 3]
+valores.sort()
+for(let pos =0;pos < valores.length;pos++) {
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+}
+console.log(`-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=`)
+for(let pos in valores){
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
 }
